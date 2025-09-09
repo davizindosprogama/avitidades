c2e3674a91354dfa3c3561adad01571d4424516b
davizindosprogama <davidosprogama@gmail.com>
2025-09-09 18:34:38 -0300

    iop
    1

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 const resultado = document.getElementById("resultado");
+const calcular = document.getElementById("calcular");
+
 let expressao = "";
 
 function limpar() {
     expressao = "";
     resultado.textContent = "0";
+}
+
+function calcular() {
+    if (op)
 }
